0b804ec639b34c1d64c362e68a490eb2f7ec4562
Simon Konstantyner <simon@konstantyner.dk>
2019-06-24 14:14:26 +0200

    added cascade to likes

--- database/migrations/2019_06_21_210630_create_likes_table.php
+++ database/migrations/2019_06_21_210630_create_likes_table.php
@@ -18,6 +18,9 @@ class CreateLikesTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('status_id');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('status_id')->references('id')->on('statuses')->onDelete('cascade');
         });
     }
 
