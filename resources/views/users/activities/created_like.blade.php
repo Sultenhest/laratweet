5bf63349fc0ea760f075ea7bd81e91e8476e4ce4
Simon Konstantyner <simon@konstantyner.dk>
2019-07-14 16:32:02 +0200

    updated liked view

--- resources/views/users/activities/created_like.blade.php
+++ resources/views/users/activities/created_like.blade.php
@@ -18,8 +18,35 @@
     @endslot
 
     @slot('body')
-        Liked:
-        <a href="/status/{{ $record->subject->liked_id }}" class="btn btn-info">Go to status</a>
+        <p>Liked the status:</p>
+        @component('users.activities.activity')
+            @slot('heading')
+                <strong>
+                    {{ $record->subject->liked->user->name }}
+                </strong>
+
+                <a href="{{ $record->subject->liked->user->path() }}">
+                    {{ $record->subject->liked->user->username }}
+                </a>
+                -
+                <small>
+                    {{ $record->created_at->toFormattedDateString() }}
+
+                    @if( $record->updated_at->gt($record->created_at) )
+                        | Modified on {{ $record->updated_at->toFormattedDateString() }}
+                    @endif
+                </small>
+            @endslot
+            @slot('body')
+                {{ $record->subject->liked->body }}
+            @endslot
+
+            @slot('bottom')
+                @if (url()->current() != 'http://laratweet.test' . $record->subject->liked->path())
+                    <a href="{{ $record->subject->liked->path() }}" class="btn btn-info">Go to status</a>
+                @endif
+            @endslot
+        @endcomponent
     @endslot
 
     @slot('bottom')
