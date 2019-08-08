8e965193567aced248e4c26a39a63dcb095cd514
Simon Konstantyner <simon@konstantyner.dk>
2019-08-08 15:41:17 +0200

    new links in header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,10 +57,18 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    Hello {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ Auth::user()->path() }}">
+                                        {{ __('To Profile') }}
+                                    </a>
+
+                                    <a class="dropdown-item" href="{{ Auth::user()->path() }}/edit">
+                                        {{ __('Edit Profile') }}
+                                    </a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
